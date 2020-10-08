4691dbc22c1bf962edea931d827bbdb766114178
magantus@googlemail.com <Extrem93!>
2020-10-08 17:57:42 +0200

    define convert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RegestrationComponent} from './regestration/regestration.component';
 import {LoginComponent} from './login/login.component';
+import {StartComponent} from './start/start.component';
 const routes: Routes = [
-  {path:'', redirectTo:'regestration-component',pathMatch:'full'},
+  {path:'', redirectTo:'start-component',pathMatch:'full'},
+  {path:'start-component', component: StartComponent },
   {path:'regestration-component', component: RegestrationComponent },
-  {path:'forum-component', component: LoginComponent},
+  {path:'login-component', component: LoginComponent},
 ];
 
 @NgModule({
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  tryToLogin(){
+    this.router.navigate(['login-component']);
+  }
+  tryToRegister(){
+    this.router.navigate(['regestration-component']);
   }
 
 }
