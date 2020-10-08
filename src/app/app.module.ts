4426f4a2e20ca686c774289cb1afd0cd51497f29
magantus@googlemail.com <Extrem93!>
2020-10-08 17:28:36 +0200

    Add Login and Registration Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { LoginComponent } from './login/login.component';
+import { RegestrationComponent } from './regestration/regestration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeScreenComponent
+    HomeScreenComponent,
+    LoginComponent,
+    RegestrationComponent
   ],
   imports: [
     BrowserModule,
